0bc6cda473a497200e03a688de9b000a65614279
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-10 12:55:48 +0100

    Refactor into class

--- cec_client.py
+++ cec_client.py
@@ -2,7 +2,6 @@ import cec
 
 import os
 import signal
-from time import sleep
 from subprocess import run
 
 
@@ -27,37 +26,43 @@ class Keycode:
 def main():
     print("Initializing...")
     os.environ.setdefault('DISPLAY', ':0')
-    cec.init()
-    cec.set_active_source()
-    cec.add_callback(on_keypress, cec.EVENT_KEYPRESS)
+    client = Client()
+    client.connect()
     print("Ready")
     signal.pause()
 
 
-def on_keypress(event, keycode, duration):
-    if duration != 0:
-        if keycode == Keycode.Up:
-            xdo("key", "Up")
-        elif keycode == Keycode.Down:
-            xdo("key", "Down")
-        elif keycode == Keycode.Left:
-            xdo("key", "Left")
-        elif keycode == Keycode.Right:
-            xdo("key", "Right")
+class Client:
 
-        elif keycode == Keycode.Ok:
-            xdo("key", "Return")
-        elif keycode == Keycode.Back:
-            xdo("key", "Escape")
+    def connect(self):
+        cec.init()
+        cec.set_active_source()
+        cec.add_callback(self.on_keypress, cec.EVENT_KEYPRESS)
 
-        elif keycode == Keycode.Red:
-            xdo("key", "Super_L")
-        elif keycode == Keycode.Green:
-            run(["kodi"])
-        elif keycode == Keycode.Blue:
-            run(["chromium-browser"])
-        elif keycode == Keycode.Yellow:
-            run(["shutdown", "-h", "now"])
+    def on_keypress(self, event, keycode, duration):
+        if duration != 0:
+            if keycode == Keycode.Up:
+                xdo("key", "Up")
+            elif keycode == Keycode.Down:
+                xdo("key", "Down")
+            elif keycode == Keycode.Left:
+                xdo("key", "Left")
+            elif keycode == Keycode.Right:
+                xdo("key", "Right")
+
+            elif keycode == Keycode.Ok:
+                xdo("key", "Return")
+            elif keycode == Keycode.Back:
+                xdo("key", "Escape")
+
+            elif keycode == Keycode.Red:
+                xdo("key", "Super_L")
+            elif keycode == Keycode.Green:
+                run(["kodi"])
+            elif keycode == Keycode.Blue:
+                run(["chromium-browser"])
+            elif keycode == Keycode.Yellow:
+                run(["shutdown", "-h", "now"])
 
 
 def xdo(*args):
