1801768442d22bc4a2326235bd4523302dce677e
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-21 22:09:59 +0100

    Make multi-modal

--- cec_client.py
+++ cec_client.py
@@ -4,9 +4,19 @@ import os
 import signal
 import subprocess
 from functools import partial
+from enum import Enum
 
 
-class Keycode:
+class Mode(Enum):
+    Keyboard = 0
+
+
+class Event(Enum):
+    KeyDown = 0
+    KeyUp = 1
+
+
+class Keycode(Enum):
     Up = 1
     Down = 2
     Left = 3
@@ -33,24 +43,64 @@ def Xdo(*args):
 
 
 KEYBINDINGS = {
-    Keycode.Up:      Xdo("key", "Up"),
-    Keycode.Down:    Xdo("key", "Down"),
-    Keycode.Left:    Xdo("key", "Left"),
-    Keycode.Right:   Xdo("key", "Right"),
-    Keycode.Ok:      Xdo("key", "Return"),
-    Keycode.Back:    Xdo("key", "Escape"),
-    Keycode.Red:     Xdo("key", "Super_L"),
-    Keycode.Green:   Run("kodi"),
-    Keycode.Blue:    Run("chromium-browser"),
-    Keycode.Yellow:  Run("shutdown", "-h", "now"),
+    (Mode.Keyboard, Event.KeyUp): {
+        Keycode.Up:     Xdo("key", "Up"),
+        Keycode.Down:   Xdo("key", "Down"),
+        Keycode.Left:   Xdo("key", "Left"),
+        Keycode.Right:  Xdo("key", "Right"),
+        Keycode.Ok:     Xdo("key", "Return"),
+        Keycode.Back:   Xdo("key", "Escape"),
+        Keycode.Red:    Xdo("key", "Super_L"),
+        Keycode.Green:  Run("kodi"),
+        Keycode.Blue:   Run("chromium-browser"),
+    },
 }
+
+
+class Condition:
+
+    def __init__(self, **attrs):
+        self.attrs = attrs
+
+    def __and__(self, other):
+        return Condition(**{**self.attrs, **other.attrs})
+
+    def check(self, **attrs):
+        return all([
+            self.attrs[key] == val
+            for key, val in attrs.items()
+            if key in self.attrs
+        ])
+
+
+def make_condition(on):
+    if not isinstance(on, (tuple, list)):
+        on = [on]
+    return Condition(**{
+        x.__class__.__name__.lower(): x.value
+        for x in on
+    })
+
+
+def make_keybindings(on, action):
+    cond = make_condition(on)
+    if isinstance(action, dict):
+        return [
+            (cond & x_cond, x_do)
+            for x in action.items()
+            for x_cond, x_do in make_keybindings(*x)
+        ]
+    elif callable(action):
+        return [(cond, action)]
+    else:
+        raise TypeError(type(action))
 
 
 def main():
     print("Initializing...")
     os.environ.setdefault('DISPLAY', ':0')
-    client = Client()
-    client.keybindings = KEYBINDINGS
+    client = Client(num_modes=len(Mode.__members__))
+    client.bind(KEYBINDINGS)
     client.connect()
     print("Ready")
     signal.pause()
@@ -58,8 +108,19 @@ def main():
 
 class Client:
 
-    def __init__(self):
-        self.keybindings = {}
+    def __init__(self, *, num_modes=2):
+        self.keybindings = []
+        self.num_modes = num_modes
+        self.mode = 0
+        self.bind({(Keycode.Yellow, Event.KeyDown): self.switch_mode})
+
+    def bind(self, rules):
+        self.keybindings.extend(make_keybindings((), rules))
+
+    def switch_mode(self, mode=None):
+        if mode is None:
+            mode = self.mode + 1
+        self.mode = mode % self.num_modes
 
     def connect(self):
         cec.init()
@@ -67,10 +128,12 @@ class Client:
         cec.add_callback(self.on_keypress, cec.EVENT_KEYPRESS)
 
     def on_keypress(self, event, keycode, duration):
-        if duration != 0:
-            action = self.keybindings.get(keycode)
-            if action:
+        mode = self.mode
+        event = (Event.KeyDown if duration == 0 else Event.KeyUp).value
+        for on, action in self.keybindings:
+            if on.check(keycode=keycode, mode=mode, event=event):
                 action()
+                break
 
 
 if __name__ == '__main__':
